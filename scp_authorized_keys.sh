acedd0b3a1e2bd75a1bcb1c56520c3c49c2bc52b
mohamed ammaha <m.ammaha@outlook.com>
2024-10-14 15:48:49 +0100

    installing fuse-devel to fix bindfs installation issue

--- scp_authorized_keys.sh
+++ scp_authorized_keys.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 
-# Define the WSL directory where SSH keys are stored
-WSL_VAGRANT_DIR="/home/devops/openshift/Openshift-Vagrant-Ansible/.vagrant/machines"
-# Define the user and host for admin1
-ADMIN1_USER="vagrant"
-ADMIN1_HOST="192.168.50.23"  # Replace with the actual IP or hostname of admin1
+# Define the user and the hostnames/ports from your Vagrant SSH config
+ADMIN1_HOST="admin1"
 ADMIN1_SSH_DIR="/home/vagrant/.ssh"
 
-# Copy public keys from WSL to admin1 VM
+# Ensure the .ssh directory exists on admin1
+ssh admin1 << EOF
+  mkdir -p $ADMIN1_SSH_DIR
+  chmod 700 $ADMIN1_SSH_DIR
+EOF
+
+# Copy public keys from master1, node1, and node2 to admin1 VM
 for VM in master1 node1 node2; do
     echo "Copying SSH keys from $VM to admin1..."
     
     # Use SCP to copy the private key to admin1 VM
-    scp "$WSL_VAGRANT_DIR/$VM/virtualbox/private_key" \
-        "$ADMIN1_USER@$ADMIN1_HOST:/tmp/${VM}_private_key"
+    scp -F vagrant_ssh_config $VM:/home/vagrant/.ssh/id_rsa.pub /tmp/${VM}_id_rsa.pub
     
-    # SSH into admin1 and move the keys to the correct directory, setting the right permissions
-    ssh "$ADMIN1_USER@$ADMIN1_HOST" << EOF
-      sudo mv /tmp/${VM}_private_key $ADMIN1_SSH_DIR/${VM}_id_rsa
-      sudo chmod 600 $ADMIN1_SSH_DIR/${VM}_id_rsa
-      sudo chown $ADMIN1_USER:$ADMIN1_USER $ADMIN1_SSH_DIR/${VM}_id_rsa
+    # SSH into admin1 and append the keys to the correct directory, setting the right permissions
+    ssh -F vagrant_ssh_config admin1 << EOF
+      cat /tmp/${VM}_id_rsa.pub >> $ADMIN1_SSH_DIR/authorized_keys
+      rm -f /tmp/${VM}_id_rsa.pub
+      chmod 600 $ADMIN1_SSH_DIR/authorized_keys
+      chown vagrant:vagrant $ADMIN1_SSH_DIR/authorized_keys
 EOF
     
-    echo "Key for $VM copied and permissions set."
+    echo "Key for $VM copied and appended to authorized_keys on admin1."
 done
-
-# Append the public keys to authorized_keys on admin1
-echo "Appending keys to authorized_keys on admin1..."
-for VM in master1 node1 node2; do
-    ssh-keyscan "$VM.example.com" >> "$ADMIN1_SSH_DIR/authorized_keys"
-done
-
-# Set correct permissions for authorized_keys
-ssh "$ADMIN1_USER@$ADMIN1_HOST" << EOF
-  sudo chmod 600 $ADMIN1_SSH_DIR/authorized_keys
-  sudo chown $ADMIN1_USER:$ADMIN1_USER $ADMIN1_SSH_DIR/authorized_keys
-EOF
 
 echo "Keys copied and configured successfully on admin1."
